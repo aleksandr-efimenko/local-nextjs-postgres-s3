21931c38b3acd83c568f9c5debab63b8a49fd749
Aleksandr Efimenko <mail@alexefimenko.com>
2024-01-06 16:36:01 -0500

    Update next.config.js and prisma schema
    This commit updates the next.config.js file to include the "output" property with the value "standalone". It also updates the prisma schema to use the "postgresql" provider and adds binary targets for different platforms.

--- next.config.js
+++ next.config.js
@@ -7,6 +7,7 @@ await import("./src/env.js");
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
+  output: "standalone",
 
   /**
    * If you are using `appDir` then you must comment the below `i18n` config out.
