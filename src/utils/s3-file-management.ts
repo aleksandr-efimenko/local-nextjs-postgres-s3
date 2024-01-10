233baa4114ecd70354aa1e1438f28aa3566ee629
Aleksandr Efimenko <mail@alexefimenko.com>
2024-01-10 15:55:28 -0500

    implement s3 minio util functions

---
+++ src/utils/s3-file-management.ts
@@ -0,0 +1,75 @@
+import * as Minio from "minio";
+import type internal from "stream";
+import { env } from "~/env.js";
+
+const s3Client = new Minio.Client({
+  endPoint: env.S3_ENDPOINT,
+  port: env.S3_PORT ? Number(env.S3_PORT) : undefined,
+  accessKey: env.S3_ACCESS_KEY,
+  secretKey: env.S3_SECRET_KEY,
+  useSSL: false,
+});
+
+export async function createBucketIfNotExists(bucketName: string) {
+  const bucketExists = await s3Client.bucketExists(bucketName);
+  if (!bucketExists) {
+    await s3Client.makeBucket(bucketName);
+  }
+}
+
+export async function saveFileInBucket({
+  bucketName,
+  fileName,
+  file,
+}: {
+  bucketName: string;
+  fileName: string;
+  file: Buffer | internal.Readable;
+}) {
+  // Create bucket if it doesn't exist
+  await createBucketIfNotExists(bucketName);
+
+  // check if file exists
+  const fileExists = await checkFileExistsInBucket({
+    bucketName,
+    fileName,
+  });
+
+  if (fileExists) {
+    throw new Error("File already exists");
+  }
+
+  // Upload image to S3 bucket
+  await s3Client.putObject(bucketName, fileName, file);
+}
+
+export async function checkFileExistsInBucket({
+  bucketName,
+  fileName,
+}: {
+  bucketName: string;
+  fileName: string;
+}) {
+  try {
+    await s3Client.statObject(bucketName, fileName);
+  } catch (error) {
+    return false;
+  }
+  return true;
+}
+
+export async function getFileFromBucket({
+  bucketName,
+  fileName,
+}: {
+  bucketName: string;
+  fileName: string;
+}) {
+  try {
+    await s3Client.statObject(bucketName, fileName);
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+  return await s3Client.getObject(bucketName, fileName);
+}
