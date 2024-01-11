ed449a1b9aa225ed2dbb497021634fa85ed3c359
Aleksandr Efimenko <mail@alexefimenko.com>
2024-01-10 20:57:38 -0500

    create download route

---
+++ src/pages/api/files/download/[id].ts
@@ -0,0 +1,39 @@
+import { type NextApiRequest, type NextApiResponse } from "next";
+import { getFileFromBucket } from "~/utils/s3-file-management";
+import { env } from "~/env";
+import { db } from "~/server/db";
+
+async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const { id } = req.query;
+  const fileObject = await db.file.findUnique({
+    where: {
+      id,
+    },
+    select: {
+      fileName: true,
+      originalName: true,
+    },
+  });
+  if (!fileObject) {
+    return res.status(404).json({ message: "Item not found" });
+  }
+
+  const data = await getFileFromBucket({
+    bucketName: env.S3_BUCKET_NAME,
+    fileName: fileObject?.fileName,
+  });
+
+  if (!data) {
+    return res.status(404).json({ message: "Item not found" });
+  }
+
+  res.setHeader(
+    "content-disposition",
+    `attachment; filename="${fileObject?.originalName}"`,
+  );
+
+  // pipe the data to the res object
+  data.pipe(res);
+}
+
+export default handler;
